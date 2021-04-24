9c1c5a7d02f4e0d3104619119ab2ce0fc00bf2e4
George Fourmouzis <georgefourm@gmail.com>
2021-04-24 15:41:43 +0100

    Add intro text

---
+++ Assets/Scripts/UI/TextPanelController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextPanelController : MonoBehaviour
+{
+    public Text textRef;
+
+    public TextAsset textAsset;
+
+    public float Speed = 20;
+
+    string text;
+
+    bool completed = false;
+
+    IEnumerator coroutine;
+
+    private void Start()
+    {
+        text = textAsset.text;
+        coroutine = writeText();
+        StartCoroutine(coroutine);
+    }
+
+    protected IEnumerator writeText()
+    {
+        for (int i = 0; i < text.Length; i++)
+        {
+            textRef.text = text.Substring(0, i);
+
+            yield return new WaitForSeconds(1/Speed);
+        }
+        completed = true;
+    }
+
+    public void Skip()
+    {
+        if (completed)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            StopCoroutine(coroutine);
+            textRef.text = text;
+            completed = true;
+        }
+    }
+}
