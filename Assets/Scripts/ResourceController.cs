b0e56d6539e594472536fe2df01c0b872484fb52
Dimitris Masouras <masouras.gdim@gmail.com>
2021-04-26 10:19:09 +0300

    Add goal model and change prefab to use it. Also add constant rotation to goal and collectibles.

--- Assets/Scripts/ResourceController.cs
+++ Assets/Scripts/ResourceController.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class ResourceController : MonoBehaviour
 {
+    public float rotatingSpeed = 0.25f;
+    private float rotation;
+
+    private void Start()
+    {
+        rotation = 0f;
+    }
+
+    private void Update()
+    {
+        rotation += Time.deltaTime * 360f * rotatingSpeed;
+        transform.localRotation = Quaternion.Euler(0, rotation, 0);
+        if (rotation >= 360f) rotation -= 360f;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Player")
